d94b441415e2026383feacb0bda9f3e26acd7572
ESievierin <egor.sievierin@gmail.com>
2025-09-13 14:18:25 +0300

    MRKT-10-refactor-product-api

---
+++ Market.Products.BLL/RequestHandlers/Product/GetProductDetailsByIdQueryHandler.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using Market.Products.BLL.DTO;
+using Market.Products.BLL.Exceptions;
+using Market.Products.DAL.DbModels;
+using Market.Products.DAL.EF;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Market.Products.BLL.RequestHandlers.Product
+{
+    public record GetProductDetailsByIdQuery(int ProductId) : IRequest<ProductDetailsDto>;
+    public sealed class GetProductDetailsByIdQueryHandler(MarketProductsDbContext dbContext, IMapper mapper) : IRequestHandler<GetProductDetailsByIdQuery, ProductDetailsDto>
+    {
+        public async Task<ProductDetailsDto> Handle(GetProductDetailsByIdQuery request, CancellationToken cancellationToken)
+        {
+            var productDetails = await dbContext.Products
+                .AsNoTracking()
+                .Where(p => p.Id == request.ProductId)
+                .Select(p => new ProductDetailsDto 
+                {
+                    Description = p.Description,
+                    Category = mapper.Map<CategoryDto>(p.Category),
+                    Manufacturer = mapper.Map<ManufacturerDto>(p.Manufacturer)
+                })
+                .AsSplitQuery()
+                .FirstOrDefaultAsync(cancellationToken);
+
+            if (productDetails == null) 
+            {
+                throw new EntityNotFoundException<ProductDbModel>(request.ProductId);
+            }
+
+            return productDetails;
+        }
+    }
+}
